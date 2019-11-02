d989b111993011b0c9542ae959a530ea63b74f1d
thavoo <gustavoh.2312@gmail.com>
2019-11-02 10:25:23 -0500

    initial project java spring

---
+++ src/test/java/com/notas/core/RestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notas.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
